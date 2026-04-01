70d581de75832a65b9857f59c57d7db7a0187f8b
Rafi A <Sleekfish8@gmail.com>
2026-04-01 16:08:53 +0700

    kelas pertama

--- Pertemuan-1.cpp
+++ Pertemuan-1.cpp
@@ -2,8 +2,7 @@
 
 const int size = 3;
 
-double Average(int nums[size])
-{
+double Average(int nums[size]){
     double average = 0;
     for (int i = 0; i < size; i++)
     {
@@ -34,16 +33,39 @@ int main(int argc, char** argv){
     numbers[2] = 30;
 
 // for print array
-int firstarr[] = {1,2,3,4,5};
-int secondarr[] = {6,7,8,9,10};
+int firstarr[5];
+int secondarr[5];
+int firstLength;
+int secondLength;
 
-    std::cout << "the first array is ";
-    printarray(firstarr, 5);
-    std::cout << "the second array is ";
-    printarray(secondarr, 5);
+    std::cout << "enter the first array: ";
+    for (int i = 0; i < 5; i++)
+    {
+        std::cin >> firstarr[i];
+    }
+    std::cout << "enter the second array: ";
+    for (int i = 0; i < 5; i++)
+    {
+        std::cin >> secondarr[i];
+    }
+
+
+
+    std::cout << "the length of the first array you want to print: ";
+    std::cin >> firstLength;
+
+    std::cout << "the length of the second array you want to print: ";
+    std::cin >> secondLength;
+
+    std::cout << std::endl;
+    std::cout << "the first array is: ";
+    printarray(firstarr, firstLength);
+    std::cout << "the second array is: ";
+    printarray(secondarr, secondLength);
 
 std::cout << std::endl;
 
+// for average
     std::cout << "hello the average is " << Average(numbers) << std::endl;
 
 }
